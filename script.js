aae26369d32084b94b27c273f12593efc6c9293e
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-11-30 13:54:40 +0100

    TODO card template css

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 console.log("countries")
 
 
+let data = "";
+const countriesFetcher = function () {
+    fetch('https://restcountries.com/v3.1/all')
+        .then((response) => response.json())
+        .then((res) => {
+            data = res;
+            console.log(data)
+        })
+}
+
+countriesFetcher();
